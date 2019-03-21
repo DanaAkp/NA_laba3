84950e325f0a7f9402337e682e86768b74502a9b
Danagul Akpaeva <aidanagul10@gmail.com>
2019-03-21 13:05:51 +0300

    Добавьте файлы проекта.

---
+++ NA_laba3/Matrix.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NA_laba3
+{
+    class Matrix
+    {
+        public static double determinant;
+        public static int[] countX;
+        public static int CountTransposition = 0;
+        /// <summary>
+                                                   /// Вывод матрицы
+                                                   /// </summary>
+                                                   /// <param name="A"></param>
+                                                   /// <param name="n"></param>
+                                                   /// <param name="m"></param>
+        public static void Output(double[,] A, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    Console.Write(string.Format("{0:F2} ", A[i, j]));
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        /// <summary>
+        /// Вывод вектора
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        public static void Output(double[] A, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Format("{0:F2} ", A[i]));
+            }
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        /// <summary>
+        /// умножение матриц
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="k"></param>
+        public static double[,] MulMatrix(double[,] A, double[,] B, int n, int m, int k)
+        {
+            double[,] C = new double[n, k];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    for (int c = 0; c < m; c++)
+                    {
+                        C[i, j] += A[i, c] * B[c, j];
+                    }
+                }
+            }
+            Output(C, n, k);
+            return C;
+        }
+        /// <summary>
+        /// Умножение матрицы на вектор
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="k"></param>
+        public static double[] MulMatrix(double[,] A, double[] B, int n, int m)
+        {
+            double[] C = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    C[i] += A[i, c] * B[c];
+                }
+            }
+            Output(C, n);
+            return C;
+        }/// <summary>
+         /// Ошибка матриц
+         /// </summary>
+         /// <param name="A"></param>
+         /// <param name="n"></param>
+         /// <param name="b"></param>
+        public static void Error(double[,] A, double[,] A_1, int n)
+        {
+            double[,] sub = new double[n, n];
+            for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) sub[i, j] = Math.Abs(A[i, j] - A_1[i, j]);
+            double myB = average(A, n);
+            double b = average(sub, n);
+            Console.WriteLine("inccuracy = " + b / myB);
+            Console.WriteLine("------------------------------------------------------------------");
+        }
+        /// <summary>
+        /// Ошибка вектора
+        /// </summary>
+        /// <param name="B"></param>
+        /// <param name="B_1"></param>
+        /// <param name="n"></param>
+        public static void Error(double[] B, double[] B_1, int n)
+        {
+            double[] sub = new double[n];
+            for (int i = 0; i < n; i++) sub[i] = Math.Abs(B[i] - B_1[i]);
+            double b = average(B, n);
+            double new_b = average(sub, n);
+            Console.WriteLine("inccuracy = " + new_b / b);
+        }
+        /// <summary>
+        /// Среднее квадратичное вектора
+        /// </summary>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double average(double[] B, int n)
+        {
+            double delta = 0;
+            for (int i = 0; i < n; i++)
+                delta += B[i] * B[i];
+            return delta = Math.Sqrt(delta);
+        }
+        /// <summary>
+        /// Среднее квадратичное матрицы
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double average(double[,] A, int n)
+        {
+            double delta = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    delta += A[i, j] * A[i, j];
+            }
+
+            delta = Math.Sqrt(delta);
+            return delta;
+        }
+        /// <summary>
+        /// Из квадратной матрицы А получаем прямоугольную путем добавления столбца В
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double[,] ExtendedMatrix(double[,] A, double[] B, int n)
+        {
+            double[,] newA = new double[n, n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    if (j == n) newA[i, j] = B[i];
+                    else newA[i, j] = A[i, j];
+                }
+            }
+            return newA;
+        }
+        /// <summary>
+        /// Получение столбца В из расширенной матрицы
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static double[] GetBFromExtendedMatrix(double[,] A, int n, int m)
+        {
+            double[] B = new double[n];
+            for (int i = 0; i < n; i++)
+                B[i] = A[i, m - 1];
+            return B;
+        } /// <summary>
+          /// 
+          /// </summary>
+          /// <param name="A"></param>
+          /// <param name="n"></param>
+          /// <param name="a">Индекс строки, которую нужно заменить на строку с индекс К</param>
+          /// <param name="k">Индекс строки, которую нужно заменить на строку индекс А</param>
+          /// <returns></returns>
+        public static double[,] Colum(double[,] A, int n, int a, int k)
+        {
+            for (int i = 0; i < n + 1; i++)
+            {
+                double buf = A[a, i];
+                A[a, i] = A[k, i];
+                A[k, i] = buf;
+            }
+            return A;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <param name="a">Индекс столбца</param>
+        /// <param name="k">Индекс столбца</param>
+        /// <returns></returns>
+        public static double[,] Row(double[,] A, int n, int a, int k)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                double buf = A[i, a];
+                A[i, a] = A[i, k];
+                A[i, k] = buf;
+            }
+            int c = countX[a];
+            countX[a] = countX[k];
+            countX[k] = c;
+            return A;
+        }
+    }
+}
---
+++ NA_laba3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NA_laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 4;
+            double[,] A1 = new double[n, n] { { 2, 0.2, -1, 0 }, { 0.4, -8.5, 0.5, 4 }, { 0.3, -1, 5.2, 1 }, { 1, 0.2, -1, 2.5 } };
+            double[] d = new double[n] { 2.7, 21.9, -3.9, 9.9 };
+
+            //double[,] A1 = new double[n, n] { { -0.86, 0.23, 0.18, 0.17 }, { 0.12, -1.14, 0.08, 0.09 }, { 0.16, 0.24, -1, -0.35 }, { 0.23, -0.08, 0.05, -0.75 } };
+            //double[] d = new double[n] { 1.42, 0.83, -1.21, -0.65 };
+
+            double[,] A = Matrix.ExtendedMatrix(A1, d, n);
+
+
+        }
+        public static bool IsDiagonalDominance(double[,] A, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < n; j++) if (i != j) sum += A[i, j];
+                if (sum > A[i, i]) return false;
+            }
+            return true;
+        }
+
+        public static double GetDeterminant_3(double[,] A, int n)
+        {
+            return A[0, 0] * A[1, 1] * A[2, 2] + A[0, 1] * A[1, 2] * A[2, 0] + A[1, 0] * A[2, 1] * A[0, 2] - A[2, 0] * A[1, 1] * A[0, 2] - A[1, 0] * A[0, 1] * A[2, 2] - A[2, 1] * A[1, 2] * A[0, 0];
+        }
+        public static double[,] GetMinor(double[,] A, int n, int i,int j)
+        {
+            double[,] M = new double[n - 1, n - 1];
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int m = 0; m < n; m++)
+                {
+                    if (i != k && m != j)
+                    {
+                        if (i < k) M[k, m] = A[k, m];
+                    }
+                }
+            }
+
+            return M;
+        }
+        public static double GetDeterminant_4(double[,] A, int n)
+        {
+            double determ = 1;
+            for(int i = 0; i < n; i++)
+            {
+
+            }
+            return determ;
+        }
+        public static double[,] GetDiagonalDominance(double[,] A,int n)
+        {
+
+            return A;
+        }
+    }
+}
